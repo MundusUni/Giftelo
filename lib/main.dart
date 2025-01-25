ab9beed4ab049578808c7ed0ec2aa22e529ad2a6
MundusUni <94185585+MundusUni@users.noreply.github.com>
2025-01-25 10:32:20 -0300

    v0.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,21 @@ class BarbeiroApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true, // Mantém Material 3 ativado
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          //backgroundColor: Colors.black, // Define a cor de fundo da BottomNavigationBar
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+        ),
+      ),
       home: HomeScreen(),
     );
   }
 }
+
+//flutter clean
+//flutter pub get
+//flutter build apk --debug
+//flutter run --no-enable-impeller
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,47 +1,59 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super (key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeState extends State<HomeScreen> {
-  int currentPageIndex = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  List<Widget> body = [
+    Icon(Icons.menu),
+    Icon(Icons.question_mark),
+    Icon(Icons.add),
+    Icon(Icons.settings),
+    Icon(Icons.badge),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.grey,
-        selectedIndex: currentPageIndex,
-      destinations: const <Widget>[
-        NavigationDestination(
-          icon: Icon(Icons.menu), 
-          label: 'Lista de Cadastrados',
-          ),
-        NavigationDestination(
-          icon: Icon(Icons.question_mark), 
+    return Scaffold(
+  bottomNavigationBar: BottomNavigationBar(
+  type: BottomNavigationBarType.fixed, // Importante para mostrar a cor de fundo
+  backgroundColor: Colors.black54,
+  currentIndex: _currentIndex,
+  onTap: (int newIndex) {
+    setState(() {
+      _currentIndex = newIndex;
+    });
+  },
+  selectedItemColor: Colors.white,
+  unselectedItemColor: Colors.black,
+  items: const [
+        BottomNavigationBarItem(
+          label: 'Lista',
+          icon: Icon(Icons.menu),
+        ),
+        BottomNavigationBarItem(
           label: 'Tutorial',
-          ),
-        NavigationDestination(
-          icon: Icon(Icons.add), 
-          label: 'Adicionar Novo',
-          ),
-        NavigationDestination(
-          icon: Icon(Icons.badge), 
+          icon: Icon(Icons.question_mark),
+        ),
+        BottomNavigationBarItem(
+          label: 'Adicionar',
+          icon: Icon(Icons.add),
+        ),
+        BottomNavigationBarItem(
           label: 'Layouts',
-          ),
-        NavigationDestination(
-          icon: Icon(Icons.settings), 
+          icon: Icon(Icons.badge),
+        ),
+        BottomNavigationBarItem(
           label: 'Configurações',
-          ),
-        ]
-      );
-  }
+          icon: Icon(Icons.settings),
+        ),
+      ],
+    ),
+  );
+}
 }
