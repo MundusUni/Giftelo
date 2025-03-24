cfc2e54b908a99c00c798e9fe553944fe141d27e
MundusUni <94185585+MundusUni@users.noreply.github.com>
2025-03-24 10:50:43 -0300

    Create dadatabasegpt.dart

---
+++ lib/dadatabasegpt.dart
@@ -0,0 +1,43 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  DatabaseHelper._internal();
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final path = await getDatabasesPath();
+    final databasePath = join(path, 'app_database.db');
+
+    return await openDatabase(
+      databasePath,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        phone TEXT,
+        layout TEXT,
+        usage_count INTEGER DEFAULT 0
+      )
+    ''');
+  }
+}
