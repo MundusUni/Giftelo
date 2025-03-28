ad6684f415b63b67159111a68db5a990529282df
MundusUni <94185585+MundusUni@users.noreply.github.com>
2025-03-28 18:41:05 -0300

    V0.0.3
    Incrementado banco de dados corretamente, e adicionados botões de salvar, adicionar, e deletar, de forma funcional.

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper._internal();
@@ -14,6 +16,29 @@ class DatabaseHelper {
     return _database!;
   }
 
+  
+  //Este código é para ser ativado com o código abaixo no lista_de_cadastrados.dart:
+  /*
+DatabaseHelper dbHelper = DatabaseHelper();
+await dbHelper.dropUsersTable();
+  */
+  Future<void> dropUsersTable() async {
+  final db = await database;
+  await db.execute('DROP TABLE IF EXISTS users');
+  // Recriar a tabela após dropar
+  await db.execute('''
+    CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      celular TEXT,
+      layout TEXT,
+      usos INTEGER DEFAULT 0,
+      max_usos INTEGER
+    )
+  ''');
+  }
+
+
   Future<Database> _initDatabase() async {
     final path = await getDatabasesPath();
     final databasePath = join(path, 'usuarios.db');
@@ -26,29 +51,94 @@ class DatabaseHelper {
           CREATE TABLE users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
-            phone TEXT,
+            celular TEXT,
             layout TEXT,
-            max_usos INTEGER,
-            usos INTEGER DEFAULT 0
+            usos INTEGER DEFAULT 0,
+            max_usos INTEGER
           )
         ''');
       },
     );
   }
 
-  Future<int> addUser(String name, String phone, String layout, int maxUsos) async {
+
+  Future<int> addUser(Map<String, dynamic> user) async {
+    final db = await database;
+    return await db.insert('users', user,conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+
+/*
+  Future<int> addUser(String name, String phone, String layout, int maxUsos, int usos) async {
     final db = await database;
     return await db.insert('users', {
       'name': name,
       'phone': phone,
       'layout': layout,
+      'usos': usos,
       'max_usos': maxUsos,
-      'usos': 0,
     });
   }
+*/
 
+Future<int> deleteUser(int id) async {
+  final db = await database;
+  return await db.delete('users', where: 'id = ?', whereArgs: [id]);
+}
+
+
+Future<int> updateUser(Map<String, dynamic> user) async {
+  final db = await database;
+  return await db.update(
+    'users',
+    user,
+    where: 'id = ?',
+    whereArgs: [user['id']],
+  );
+}
+
+/*
+Future<int> updateUser(int id, String name, String phone, String layout, int maxUsos, int usos) async {
+  final db = await database;
+  return await db.update(
+    'users',
+    {'name': name, 'phone': phone, 'layout': layout, 'max_usos': maxUsos, 'usos': usos},
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+}
+*/
+
+
+Future<List<Map<String, dynamic>>> getAllUsers() async {
+  final db = await database;
+  // Query para retornar todos os usuários
+  List<Map<String, dynamic>> users = await db.query('users');
+
+  // Ajuste para garantir que a estrutura dos dados retorne como esperado
+  return users.map((user) {
+    return {
+      'id': user['id'], // id do usuário
+      'name': user['name'], // nome do usuário
+      'celular': user['celular'], // telefone do usuário
+      'layout': user['layout'], // número do layout
+      'usos': user['usos'], // número de usos
+      'max_usos': user['max_usos'], // número máximo de usos
+    };
+  }).toList();
+}
+}
+
+/*
   Future<List<Map<String, dynamic>>> getAllUsers() async {
     final db = await database;
     return await db.query('users');
   }
 }
+*/
+
+
+
+
+
+
--- lib/ui/lista_de_cadastrados.dart
+++ lib/ui/lista_de_cadastrados.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'database_helper.dart';
+import '../data/database_helper.dart';
 
 class ListaDeCadastrados extends StatefulWidget {
   const ListaDeCadastrados({Key? key}) : super(key: key);
@@ -12,10 +12,12 @@ class _ListaDeCadastradosState extends State<ListaDeCadastrados> {
   List<Map<String, dynamic>> users = [];
   final _dbHelper = DatabaseHelper();
 
-  final _nomeController = TextEditingController();
+  final _nameController = TextEditingController();
   final _celularController = TextEditingController();
   final _layoutController = TextEditingController();
+  final _usosController = TextEditingController(text: '0');
   final _maxUsosController = TextEditingController(text: '10');
+
 
   @override
   void initState() {
@@ -30,6 +32,36 @@ class _ListaDeCadastradosState extends State<ListaDeCadastrados> {
     });
   }
 
+
+
+
+
+
+
+ Future<void> _adicionarCliente() async {
+    if (_nameController.text.isEmpty) return;
+
+  // Criação do Map com os dados
+  Map<String, dynamic> user = {
+    'name': _nameController.text,
+    'celular': _celularController.text,
+    'layout': _layoutController.text,
+    'usos': int.parse(_usosController.text),
+    'max_usos': int.parse(_maxUsosController.text),
+  };
+    await _dbHelper.addUser(user); // Passando o Map para o método addUser
+    // Limpando os campos após adicionar o cliente
+    _nameController.clear();
+    _celularController.clear();
+    _layoutController.clear();
+    _usosController.text = '0';
+    _maxUsosController.text = '10';
+    Navigator.pop(context);
+    _loadUsers(); // Atualizar a lista após adicionar
+  }
+
+
+/*
   Future<void> _adicionarCliente() async {
     if (_nomeController.text.isEmpty) return;
 
@@ -37,22 +69,27 @@ class _ListaDeCadastradosState extends State<ListaDeCadastrados> {
       _nomeController.text,
       _celularController.text,
       _layoutController.text,
-      int.parse(_maxUsosController.text),
+      int.parse(_usosController.text),
+      int.parse(_maxUsosController.text)
     );
 
     _nomeController.clear();
     _celularController.clear();
     _layoutController.clear();
+    _usosController.text = '0';
     _maxUsosController.text = '10';
 
     Navigator.pop(context);
     _loadUsers(); // Atualizar a lista após adicionar
   }
+*/
+
 
   void _mostrarPopupNovoCliente() {
-    _nomeController.clear();
+    _nameController.clear();
     _celularController.clear();
     _layoutController.clear();
+    _usosController.text = '0';
     _maxUsosController.text = '10';
 
     showDialog(
@@ -63,7 +100,7 @@ class _ListaDeCadastradosState extends State<ListaDeCadastrados> {
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              _buildTextField('Nome', _nomeController),
+              _buildTextField('Name', _nameController),
               _buildTextField('Celular', _celularController),
               _buildTextField('Layout', _layoutController),
               _buildTextField('Número Máximo de Usos', _maxUsosController, keyboardType: TextInputType.number),
@@ -80,6 +117,126 @@ class _ListaDeCadastradosState extends State<ListaDeCadastrados> {
     );
   }
 
+void _mostrarPopupEdicao(Map<String, dynamic> usuario) {
+  _nameController.text = usuario['name'];
+  _celularController.text = usuario['celular'];
+  _layoutController.text = usuario['layout'];
+  _usosController.text = usuario['usos'].toString(); // Preenche os usos
+  _maxUsosController.text = usuario['max_usos'].toString();
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Editar Cliente', textAlign: TextAlign.center),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _buildTextField('Name', _nameController),
+            _buildTextField('Celular', _celularController),
+            _buildTextField('Layout', _layoutController),
+            _buildTextField('Usos', _usosController),
+            _buildTextField('Número Máximo de Usos', _maxUsosController, keyboardType: TextInputType.number),
+          ],
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // Alinha os botões
+              children:[
+                 // Botão Deletar à esquerda
+                TextButton(
+                  onPressed: () { 
+                    _confirmarExclusao(usuario['id']); // Chama a confirmação de exclusão
+                  },
+                  child: const Text('Deletar', style: TextStyle(color: Colors.red)),
+                ),
+
+                // Botão Salvar ao centro
+                ElevatedButton(
+                  onPressed: () {
+                    _salvarEdicao(usuario['id']); // Chama a função para salvar
+                    Navigator.pop(context); // Fecha a popup de edição
+                  },
+                  child: const Text('Salvar'),
+                ),
+
+                // Botão Adicionar (Aumenta o número de usos)
+                TextButton(
+                  onPressed: () async {
+                  // Atualiza o campo de usos no banco
+                  await _dbHelper.updateUser({
+                    'id': usuario['id'],
+                    'name': _nameController.text,
+                    'celular': _celularController.text,
+                    'layout': _layoutController.text,
+                    'usos': usuario['usos'] + 1, // Aumenta o valor de 'usos' em 1
+                    'max_usos': int.parse(_maxUsosController.text),
+                  });
+                  _loadUsers(); // Atualiza a lista de usuários
+                  Navigator.pop(context); // Fecha a popup de edição
+                },
+                child: Text('Adicionar', style: TextStyle(color: Colors.green)),
+                ),
+              ],
+          )
+        ],
+      );
+    },
+  );
+}
+
+
+  Future<void> _salvarEdicao(int id) async {
+    try {
+      // Criação do Map com os dados
+      Map<String, dynamic> user = {
+        'id': id,
+        'name': _nameController.text,
+        'celular': _celularController.text,
+        'layout': _layoutController.text,
+        'usos': int.parse(_usosController.text),
+        'max_usos': int.parse(_maxUsosController.text),
+      };
+      await _dbHelper.updateUser(user);
+      //Navigator.pop(context);
+      _loadUsers(); // Atualizar a lista de usuários na tela
+    } catch (e) {
+      print('Deu erro né...');
+    }
+  }
+
+
+void _deleteUser(int id) async {
+  await _dbHelper.deleteUser(id); // Deleta o usuário do banco de dados
+  _loadUsers(); // Atualiza a lista de usuários na tela
+}
+
+void _confirmarExclusao(int id) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Confirmar Exclusão'),
+        content: const Text('Tem certeza que deseja excluir este usuário? Esta ação não pode ser desfeita.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context), // Fecha o alerta
+            child: const Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              _deleteUser(id); // Deleta o usuário
+              Navigator.pop(context); // Fecha a popup de confirmação
+              Navigator.pop(context); // Fecha a popup de edição (caso esteja aberta)
+            },
+            child: Text('Excluir', style: TextStyle(color: Colors.red)),
+          ),
+        ],
+      );
+    },
+  );
+}
+
   Widget _buildTextField(String label, TextEditingController controller, {TextInputType? keyboardType}) {
     return TextField(
       controller: controller,
@@ -88,43 +245,49 @@ class _ListaDeCadastradosState extends State<ListaDeCadastrados> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Lista de Cadastrados')),
-      body: users.isEmpty
-          ? const Center(child: Text('Nenhum usuário cadastrado'))
-          : ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 0),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(users[index]['name']),
-                      Text(
-                        '${users[index]['usos']}/${users[index]['max_usos']}',
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                  trailing: const Icon(Icons.arrow_forward_ios),
-                );
-              },
-            ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _mostrarPopupNovoCliente,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('Lista de Cadastrados')),
+    body: users.isEmpty
+        ? const Center(child: Text('Nenhum usuário cadastrado'))
+        : ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 0),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(users[index]['name']),
+                    Text(
+                      '${users[index]['usos']}/${users[index]['max_usos']}',
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+                trailing: const Icon(Icons.edit),
+                onTap: () => _mostrarPopupEdicao(users[index]),
+              );
+            },
+          ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: _mostrarPopupNovoCliente,
+      child: const Icon(Icons.add),
+    ),
+  );
+}
+
+
+
+
 
   @override
   void dispose() {
-    _nomeController.dispose();
+    _nameController.dispose();
     _celularController.dispose();
     _layoutController.dispose();
+    _usosController.dispose();
     _maxUsosController.dispose();
     super.dispose();
   }
